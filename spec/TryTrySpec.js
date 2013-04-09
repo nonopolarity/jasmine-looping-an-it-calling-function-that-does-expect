557cf14143a155933aab5fa2fc9422b98b973e56
Kin iMac <winterheat@gmail.com>
2013-04-08 21:21:31 -0700

    merged

--- spec/TryTrySpec.js
+++ spec/TryTrySpec.js
@@ -1,17 +1,24 @@
 describe("TryTry", function() {
 
-	var i;
+    var i;
 
-	for (i = 0; i < 5; i++) {
-		console.log("HERE", i); 
+    function checkForSituations(a) {
+        // say, if this is made into a function because there are a lot of processing
 
-		it("should add", function() {
+        console.log("THERE", a); 
+        expect(foo(3, a)).toEqual( 3 + a );
+    }
 
-			expect(foo(3, i)).toEqual( 3 + i );
+    for (i = 0; i < 5; i++) {
+        console.log("HERE", i); 
 
-		});
+        it("should add for " + i, function() {
 
-	}
+            checkForSituations(i);
+
+        });
+
+    }
 
 });
 
